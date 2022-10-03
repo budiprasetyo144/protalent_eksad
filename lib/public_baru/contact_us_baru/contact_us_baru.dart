7c7a5bfb49bdd6e8535164d059e887c9acd64be1
Budi Prasetyo <b.prasetyo@eksad.com>
2022-10-03 13:12:45 +0700

    hilangin appbar pas discroll

--- lib/public_baru/contact_us_baru/contact_us_baru.dart
+++ lib/public_baru/contact_us_baru/contact_us_baru.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 // import 'package:protalent_eksad/public/contact_us/ContactUs.dart';
 // import 'package:protalent_eksad/public_baru/career/career3.dart';
 import 'package:protalent_eksad/public_baru/contact_us_baru/contact_us1.dart';
@@ -30,19 +31,75 @@ class ContactUs_baru extends StatefulWidget {
 
 class _ContactUsState extends State<ContactUs_baru> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  bool _showAppbar = true;
+  ScrollController controller2 = new ScrollController();
+  bool _show = true;
+  bool isScrollingDown = false;
+
+  @override
+  void initState() {
+    super.initState();
+    myScroll();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller2.removeListener(() {});
+    super.dispose();
+  }
+
+  void showBottomBar() {
+    setState(() {
+      _show = true;
+    });
+  }
+
+  void hideBottomBar() {
+    setState(() {
+      _show = false;
+    });
+  }
+
+  void myScroll() async {
+    controller2.addListener(() {
+      if (controller2.position.userScrollDirection == ScrollDirection.reverse) {
+        if (!isScrollingDown) {
+          isScrollingDown = true;
+          _showAppbar = false;
+          hideBottomBar();
+        }
+      }
+      if (controller2.position.userScrollDirection == ScrollDirection.forward) {
+        if (isScrollingDown) {
+          isScrollingDown = false;
+          _showAppbar = true;
+          showBottomBar();
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     setPageTitle('Contact Us', context);
     var screenSize = MediaQuery.of(context).size;
     return Scaffold(
         key: _scaffoldKey,
-        appBar: ResponsiveWidget.isSmallScreen(context)
-            ? AppBarKecil()
-            : AppbarHomeLarge(screenSize, context, Colors.black, Colors.black,
-                Colors.black, Colors.black, Colors.blue),
+        appBar: _showAppbar
+            ? ResponsiveWidget.isSmallScreen(context)
+                ? AppBarKecil()
+                : AppbarHomeLarge(screenSize, context, Colors.black,
+                    Colors.black, Colors.black, Colors.black, Colors.blue)
+            : PreferredSize(
+                child: Container(),
+                preferredSize: Size(0.0, 0.0),
+              ),
         drawer: DrawerProtalent(),
         body: ResponsiveWidget.isSmallScreen(context)
             ? ListView(
+                controller: controller2,
                 children: [
                   const ContactUs1_small(),
                   ContactUs2_small(),
@@ -50,6 +107,7 @@ class _ContactUsState extends State<ContactUs_baru> {
                 ],
               )
             : ListView(
+                controller: controller2,
                 children: [
                   const ContactUs1(),
                   ContactUs2(),
